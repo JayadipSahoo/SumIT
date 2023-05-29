5e4009618fc771808830a48139cf3e9d92f155e5
Jaya-Shinra Tensei <102919369+JayadipSahoo@users.noreply.github.com>
2023-05-29 19:11:48 +0530

    fix: API related errors

--- src/services/article.js
+++ src/services/article.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 
-const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
+const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE;
 
 
 
